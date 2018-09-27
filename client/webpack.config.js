15d2517e7f96668c343a98078b6b841f1a4d43c6
Stephen McCurry <mccurrs@amazon.com>
2018-09-27 21:39:17 +0100

    fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,7 +24,6 @@ module.exports = (env) => {
         module: {
             rules: [
                 { test: /\.html$/i, use: 'html-loader' },
-                { test: /\.css$/i, use: ['style-loader', 'css-loader' + (isDevBuild ? '' : '?minimize')] },
                 {
                     test: /\.js$/i, exclude: /node_modules/, use: {
                         loader: 'babel-loader',
